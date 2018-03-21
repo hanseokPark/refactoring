d3c95d545be0408f4cee0b46c26e798a841a84cf
hanseok Park <es12850@naver.com>
2018-03-21 09:42:13 +0900

    chap03 변경전

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -3,17 +3,48 @@ package refactoring;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 
 import refactoring.chap01.Robot;
 import refactoring.chap02.FindInt;
 import refactoring.chap02.simple.SimpleDatabase;
+import refactoring.chap03.SortSample;
 
 public class Main {
+	private static final Random random = new Random(1234);
+	
+	private static void execute(int length) {
+		//난수로 데이터 작성
+		int[] data = new int[length];
+		for(int i=0; i<data.length; i++) {
+			data[i] = random.nextInt(data.length);
+		}
+		
+		//데이터 표시
+		SortSample sorter = new SortSample(data);
+		System.out.println("BEFORE:" + sorter);
+		
+		//정렬해서 표시
+		sorter.sort();
+		System.out.println(" AFTER:" + sorter);
+		
+		System.out.println();
+	}
 
 	public static void main(String[] args) {
 //		testChap01();
 //		testChap02();
-		testChap02Simple();
+//		testChap02Simple();
+		testchap03();	
+	
+	}
+
+	private static void testchap03() {
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
 	}
 
 	private static void testChap02Simple() {
