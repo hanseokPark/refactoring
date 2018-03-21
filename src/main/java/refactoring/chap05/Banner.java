fea5e5543d7af06ded37a37fd19812b8415438f6
hanseok Park <es12850@naver.com>
2018-03-21 11:47:47 +0900

    chap05 리팩토링 후

--- src/main/java/refactoring/chap05/Banner.java
+++ src/main/java/refactoring/chap05/Banner.java
@@ -6,26 +6,26 @@ public class Banner {
 	public Banner(String content) {
 		_content = content;
 	}
-	
+	//template method
 	public void print(int times) {
+		printBorder();		
+		printContent(times);		
+		printBorder();	
+	}
+
+	private void printContent(int times) {
+		//내용 출력
+		for(int i=0; i < times; i++) {
+			System.out.println("|" + _content + "|");
+		}
+	}
+
+	private void printBorder() {
 		//테투리 출력
 		System.out.print("+");
 		for(int i=0; i < _content.length(); i++) {
 			System.out.print("-");
 		}
 		System.out.println("+");
-		
-		//내용 출력
-		for(int i=0; i < times; i++) {
-			System.out.println("|" + _content + "|");
-		}
-		
-		//테두리 출력
-		System.out.print("+");
-		for(int i=0; i < _content.length(); i++) {
-			System.out.print("-");
-		}
-		System.out.println("+");
-	
 	}
 }
